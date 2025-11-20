0321de8d0b0b9765981db94ff88a4f6642c3f294
Alexander <alexander@playgrouplatam.com>
2025-11-20 14:48:27 -0500

    fix:alejandro el unico

--- infrastructure/scripts/default.sh
+++ infrastructure/scripts/default.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -ex
 
-# 1️⃣ Limpiar caches de Laravel
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
+npm run build 
 
-# 2️⃣ Migrar base de datos (esperar a que Postgres esté listo)
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
-  echo "Waiting for database..."
-  sleep 2
-done
-
-php artisan migrate --force
 
 # 4️⃣ Arrancar PHP-FPM
-php-fpm
+php-fpm &
