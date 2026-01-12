3023becd390cfd87044cea8fbfc5031d5588a0e4
Alejandro Pachas <alejandropachas1@gmail.com>
2026-01-12 14:20:51 -0500

    types/index: edited

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -17,16 +17,6 @@ export interface NavGroup {
 }
 interface BaseNavItem {
     title: string;
-}
-
-export interface NavChild {
-    title: string;
-    href: NonNullable<InertiaLinkProps['href']>;
-}
-
-export interface NavItem {
-    title: string;
-    href?: NonNullable<InertiaLinkProps['href']>;
     icon?: LucideIcon | null;
     children?: NavChild[];
 }
